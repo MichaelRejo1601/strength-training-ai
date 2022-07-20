bfd4a6aa74b82c957a421ec2dcfe045eb1086b64
Michael Rejo <ninjapenguin1601@gmail.com>
2022-07-20 12:17:10 -0700

    Rough drafted models (incomplete), started settings.
    Still needs settings completion, templates and statics folders, models completion, url schemes, login system

--- stai/stai/settings.py
+++ stai/stai/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = '/login'
+LOGIN_URL = '/login'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
--- stai/tracker/models.py
+++ stai/tracker/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Exercise(models.Model):
+    name = 
+class Log(models.Model):
+    user =
+class Workout(models.Model):
+    date = 
+class Set(models.Model):
+    workout = 
+    exercise = 
+    reps = 
+    weight = 
+class PlannedWorkout(models.Model):
+    date = 
+class PlannedSet(models.Model):
+    planned_workout = 
+    exercise = 
+    reps = 
